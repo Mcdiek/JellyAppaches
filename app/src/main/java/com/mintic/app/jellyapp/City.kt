0ebc9a8b4408d747752d6a5067655cbd38668257
Mcdiek <diegogantiva@hotmail.com>
2021-11-19 18:33:19 -0500

    city_spots.xml and ManinActivity2.kt configuration

--- app/src/main/java/com/mintic/app/jellyapp/City.kt
+++ app/src/main/java/com/mintic/app/jellyapp/City.kt
@@ -4,5 +4,6 @@ data class City (
         val cityName: String,
         val depName: String,
         var temperature: String?,
-        var imageUrl: String
+        var imageUrl: String,
+        var cityDescription: String
 )
--- app/src/main/java/com/mintic/app/jellyapp/MainActivity2.kt
+++ app/src/main/java/com/mintic/app/jellyapp/MainActivity2.kt
@@ -1,22 +1,54 @@
 package com.mintic.app.jellyapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
-import com.mintic.app.jellyapp.MainActivity.Companion.CITY_MESSAGE
+import com.bumptech.glide.Glide
+import com.mintic.app.jellyapp.MainActivity.Companion.CITY_NAME
+import com.mintic.app.jellyapp.MainActivity.Companion.CITY_TEMP
+import com.mintic.app.jellyapp.MainActivity.Companion.CITY_DESCRIPTION
+import com.mintic.app.jellyapp.MainActivity.Companion.CITY_IMAGE
+import com.mintic.app.jellyapp.MainActivity.Companion.CITY_LOCATION
+
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.city_spots)
 
-        val message = intent.getStringExtra(CITY_MESSAGE)
 
 
-        val textView = findViewById<TextView>(R.id.textView).apply {
-            text = message
+        val cityName = intent.getStringExtra(CITY_NAME)
+        val cityTemp = intent.getStringExtra(CITY_TEMP)
+        val cityDescription = intent.getStringExtra(CITY_DESCRIPTION)
+        val cityLocation = intent.getStringExtra(CITY_LOCATION)
+        val cityImageUrl = intent.getStringExtra(CITY_IMAGE)
+
+
+        val textViewTitle = findViewById<TextView>(R.id.cityName).apply {
+            text = cityName
         }
+
+        val textViewTemp = findViewById<TextView>(R.id.TextTemperatura).apply {
+            text = cityTemp
+        }
+
+        val textViewLocation = findViewById<TextView>(R.id.TextUbicacion).apply {
+            text = cityLocation
+        }
+
+        val textViewDescription = findViewById<TextView>(R.id.TextDescription).apply {
+            text = cityDescription
+        }
+
+
+        val imageView = findViewById<ImageView>(R.id.imageView);
+        Glide.with(this).load(cityImageUrl).into(imageView)
+
+
 
 
 
