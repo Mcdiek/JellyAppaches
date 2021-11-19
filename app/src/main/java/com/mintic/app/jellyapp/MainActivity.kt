5c0250c2f5c38a6b139cfc89665345e3234c1442
Mcdiek <diegogantiva@hotmail.com>
2021-11-18 21:19:40 -0500

    agrega funcionalidad al card y elimina main activity

--- app/src/main/java/com/mintic/app/jellyapp/MainActivity.kt
+++ app/src/main/java/com/mintic/app/jellyapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mintic.app.jellyapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city_menu)
     }
+
+    fun sendMessage(view: View) {
+        Log.d("TAG", "Funciona")
+        // Do something in response to button
+    }
+
+
 }
