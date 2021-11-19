f01290eb6fcebf387e3d485f43290b07d9489d87
Mcdiek <diegogantiva@hotmail.com>
2021-11-19 15:44:55 -0500

    add reciclerview functions

--- app/src/main/java/com/mintic/app/jellyapp/CityAdapter.kt
+++ app/src/main/java/com/mintic/app/jellyapp/CityAdapter.kt
@@ -1,5 +1,65 @@
 package com.mintic.app.jellyapp;
 
-class CityAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import java.util.ArrayList
 
+class CityAdapter (
+    private val mCity: ArrayList<City>,
+    private val context: Context,
+    private val onClick: (City) -> Unit
+    ) : RecyclerView.Adapter<CityAdapter.CityViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder
+        {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.city_display_layout, parent, false)
+            return CityViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holderCity: CityViewHolder, position: Int)
+        {
+            val city = mCity[position]
+            holderCity.bind(city = city)
+        }
+
+
+        //Retorna la cantidad de elementos mi lista
+        override fun getItemCount(): Int
+        {
+            return mCity.size
+        }
+
+        inner class CityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+        {
+            private var nameLabel: TextView = itemView.findViewById(R.id.text_city)
+            private var imageView: ImageView = itemView.findViewById(R.id.image_city)
+            private var currentCity: City? = null
+
+            init {
+                itemView.setOnClickListener {
+                    currentCity?.let {
+                        onClick(it)
+                    }
+                }
+            }
+
+            /* Bind Contact name and image. */
+            fun bind(city: City) {
+                currentCity = city
+
+
+                nameLabel.text = city.cityName
+
+
+                Glide.with(context)
+                    .load(city.imageUrl)
+                    .into(imageView)
+            }
+        }
 }
