c972fc6d61df367197e747ae56c209758369d8eb
Mcdiek <diegogantiva@hotmail.com>
2021-11-18 22:09:20 -0500

    add main activity 2

--- app/src/main/java/com/mintic/app/jellyapp/MainActivity.kt
+++ app/src/main/java/com/mintic/app/jellyapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.mintic.app.jellyapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +15,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_city_menu)
     }
 
+
     fun sendMessage(view: View) {
-        Log.d("TAG", "Funciona")
+
+        val textView = findViewById<TextView>(R.id.textCity1)
+        val message = textView.text.toString()
+        Log.d("TAG",message)
+        val intent = Intent(this, MainActivity2::class.java).apply {
+            putExtra(CITY_MESSAGE,message)
+        }
+
+        startActivity(intent)
         // Do something in response to button
+    }
+
+    companion object{
+        val CITY_MESSAGE = "com.mintic.app.jellyapp.MESSAGE"
     }
 
 
