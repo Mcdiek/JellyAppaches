4ad3bbee3c304dbafd127c98d5361e8ee7558446
Mcdiek <diegogantiva@hotmail.com>
2021-12-10 22:26:30 -0500

    borrar imports

--- app/src/main/java/com/mintic/app/jellyapp/CityDescriptionFragment.kt
+++ app/src/main/java/com/mintic/app/jellyapp/CityDescriptionFragment.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,7 +12,6 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import kotlin.properties.Delegates
